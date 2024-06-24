b801e2f15854466c22b6a9c70145f7423a4a45ad
saraIsmailAlFutaisi <saf660006@gmail.com>
2024-06-24 03:22:56 +0200

    first chang

---
+++ home page .php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="../program language/css/stylenew.css">
+      <link rel="stylesheet" href="css\stylenew.css">
+</head>
+<body>
+<div id="imge"> <div>
+                 <strong>language lasson<p></p> 
+                  <p></p> </strong>
+              </div></div>
+<ul>
+<li><a href="#home">login</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">language </a>
+    <div class="dropdown-content">
+      <a href="../program language/html1.php">html</a>
+      <a href="#">css</a>
+      <a href="#">php</a>
+      <a href="#">c++</a>
+      <a href="#">java script</a>
+      <a href="#">c#</a>
+      <a href="#">java</a>
+     
+    </div>
+  </li>
+  <li><a href="../program language/workshop.php">workshop</a></li>
+  <li><a href="#news">about us</a></li>
+  <li><a href="#news">evaluation</a></li>
+
+</ul>
+<section >
+<img alt="background" src="../program language/image/Programming-amico.svg " />
+</section>
+
+</body>
+<footer>
+</footer>
+</html>
